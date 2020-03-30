e1749fb02b7ed053e8de54ad2604d9537b3fd015
franciscareyes <62191671+franciscareyes@users.noreply.github.com>
2020-03-30 11:28:41 -0300

    Clase 2

--- Class_02/Script_Clase02.R
+++ Class_02/Script_Clase02.R
@@ -20,33 +20,51 @@ as.numeric(y)
 
 A<-1
 years<-2010:2020
-tiktoc<-c("Que", "linda", "te ves", "limpiando", "Esperancita")
+year <- seq(2010,2020, by = 0.5) #Va a aumentar de 0.5 en 0.5
+tiktoc<-c("Que", "linda", "te ves", "limpiando", "Esperancita",4) #C transfomra todos los elementos al mismmo tipo
+tiktoc
+
+paste("hola","mundo",sep = " ") #separa las palabaras que uno pone en ""
+paste(tiktoc, collapse = " ") #genera un string completo de la variable
+
 m1<-matrix(1:4,2,2)
-a1<-array(1:12,dim = c(2,2,3))
-d1<-data.frame(m1)
-data("quakes") # promise
+m1
+m1%*%t(m1)#La traspuesta ?? creo
+diag(m1)
+solve(m1)
+
+
+a1<-array(1:12,dim = c(2,2,3))# 2*2(filas columnas, profundidad 3)
+a1
+
+d1<-data.frame(m1) #convertir matrix en data frame
+d1
+data("quakes") # promise #data programas en r que vienen recargdados
 d1<-data.frame(quakes)
+d1
 
-ls()
-l1<-list(A=A,years,tiktoc,m1)
+#Manipulacion de objetos
+ls() #me pone en lista las variables que definimos arriba
+l1<-list(Perrito=1,years,tiktoc,m1) #perrito=A solo le estoy dando el nombre de la variable, el valor de la variable se mantiene.
+l1
 
-# Manipulación de Objetos
-ls()
+A<-1L #Para que el numero sea entero
+class(A) #numerica:decimales, integer:entero
+typeof(A) #De que tipo son los elementos dentro del objeto y como estan guardados en general, doble significa que tiene decimales
 
-class(A)
-typeof(A)
+length(years) #Largo de un objeto, de una dimension
+dim(m1) #Cuando tiene dos dimensiones como por ejemplo columnas y filas. Me marca el largo
 
-length(years)
-dim(m1)
-object.size(d1)
+object.size(d1) #me marca el tamaño de peso, memoria de mi computador que ocupa.
 
-names(d1)
-head(d1)
+names(d1)#nombre de las variables de d1
+head(d1)#cabez de la base de daros, da los 6 primeros observaciones
 tail(d1)
 
-rm(l1)
+rm(l1)#Elimina los valores
 
 #Bonus: como se borra todo?
+rm(list=ls())
 
 # Indexación uso de los []
 
@@ -54,25 +72,38 @@ length(years)
 years[1]
 
 dim(m1)
-m1[2,3]
+m1[2,2] #[fila,columna] me impima ese numero, si no pongo columna va a asumir que es la primera
 
 dim(a1)
-a1[2,1,3]
+a1
+a1[2,1,3] # [fila,columna, profundidad]
 
+#listas
+l1
 l1[2]
 l1[2][[1]][1:2]
 
+class (l1[2])
+class(l1[2][[1]])
+
+l1
+l1$Perrito#Para llamar a la variable y ver su valor
 l1[[2]][1:2]
 
-d1[1,]
-d1[,1]
+
+d1[1,] #Trabaje con todas las columnas
+d1[,1] #Trabaje con todas las filas, no aparece el nombre pq asume que usted los sabe
+d1[,'lat'] #Llamar la variable 
+d1$mag[c(1,3,5)]
+d1$mag[seq(1,16,2)]
 d1$lat[1:4]
-d1[,'lat']
-d1[1:4,c('lat','long')]
-d1$mag>5
-table(d1$mag>5)
-d1[d1$mag>6,]
-d1$dummy_5up<-as.numeric(d1$mag>5)
+d1[1:4,c('lat','long')]#pido del 1 al 4 las variables lat y long
+
+d1$mag>5#filtrar variable que cumpla con una resticcion
+table(d1$mag>5) #conteo de cuantos false y true hay
+d1[d1$mag>6,] #Utilizar la condicion para busca un indice, la coma vacia es para que nos de todas las columnas
+
+d1$dummy_5up<-as.numeric(d1$mag>5) #crear una variable nueva con lo que recien hicimos
 head(d1)
 
 # Distinguir entre funciones, objetos, números y sintaxis básica
@@ -91,15 +122,18 @@ if(A==1){
   A<-1L
 }
 
+#For loop
 
 for(i in 1:5){
   print(paste("Me le declaro a la ", i))
-  Sys.sleep(2)
+  Sys.sleep(2) 
   print("no mejor no... fail!")
   Sys.sleep(1)
 }
+#dormir el sistema por dos segundos Sys.sleep(2)
 
 i<-1
+eps<-50/(i^2)
 while(eps>0.001){
   eps<-50/(i^2)
   print(paste("eps value es still..", eps))
@@ -114,13 +148,15 @@ while(eps>0.001){
 
 quakes[quakes$mag>6,'mag']
 
-by(data = quakes$mag,INDICES = quakes$stations,FUN = mean)
-tapply(X = quakes$mag,INDEX = quakes$stations, FUN = mean)
+by(data = quakes$mag,INDICES = quakes$stations,FUN = mean) #para que me de el promedio de las magnitudes por estacion.
+tapply(X = quakes$mag,INDEX = quakes$stations, FUN = mean) #me lo imprime mas ordenado
 
-### 2. tydiverse 
+### 2. tidyverse 
 #https://rstudio.com/wp-content/uploads/2015/02/data-wrangling-cheatsheet.pdf
 library(tidyverse)
 #Cómo se instala el paquete si no lo tengo? Tank!!! ayudaaaa!
+
+install.packages("tidyverse")
 
 quakes %>% 
   filter(mag>6) %>% 
@@ -136,6 +172,11 @@ library(data.table)
 #https://github.com/rstudio/cheatsheets/raw/master/datatable.pdf
 
 quakes<-data.table(quakes)
+ 
+quakes[quakes$mag>6,'mag']
+
+quakes[mag>6,]
+quakes[mag>6,.(mag,depth)] #.() para hacer listas 
 
 quakes[mag>6,.(mag)]
 
